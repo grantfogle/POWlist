06b7aa4679da5c8ca1e8fd7b8ee009aaf8109750
Grant <grant.fogle@davita.com>
2020-10-28 16:41:07 -0600

    added resort filter

--- src/app/resorts/filter/filter.component.ts
+++ src/app/resorts/filter/filter.component.ts
@@ -18,8 +18,17 @@ import {
 export class FilterComponent implements OnInit, OnChanges {
   @ViewChild('filterInput') filterInput: ElementRef;
   @Output() filterResorts = new EventEmitter<string>();
+  @Output() ikonFilterSelected = new EventEmitter<boolean>();
+  @Output() passFilterSelected = new EventEmitter<string>();
   displayFilters = false;
-  ikonSelected = true;
+  ikon = false;
+  epic = false;
+  mtnCollective = false;
+  familyFriendly = false;
+  powder = false;
+  bigMtn = false;
+  affordable = false;
+  filters = [];
   
   constructor() {
     console.log('Contructor called');
@@ -39,7 +48,44 @@ export class FilterComponent implements OnInit, OnChanges {
     this.filterResorts.emit(this.filterInput.nativeElement.value);
   }
 
-  toggleFilterView() {
+  selectIkon() {
+    console.log('ikon selected')
+    this.ikon = !this.ikon;
+    if (this.ikon) {
+      this.ikonFilterSelected.emit(true);
+    }
+  }
+
+  filterByPasses(pass) {
+    console.log(pass);
+    if (pass === 'Mtn Collective') {
+      this.epic = false;
+      this.ikon = false;
+      this.mtnCollective = !this.mtnCollective;
+      if (this.mtnCollective) {
+        this.passFilterSelected.emit(pass);
+      }
+    } else if (pass === 'Ikon') {
+      this.epic = false;
+      this.ikon = !this.ikon;
+      this.mtnCollective = false;
+      if (this.ikon) {
+        this.passFilterSelected.emit(pass);
+      }
+    } else if (pass === 'Epic') {
+      this.epic = !this.epic;
+      this.ikon = false;
+      this.mtnCollective = false;
+      if (this.epic) {
+        this.passFilterSelected.emit(pass);
+      } 
+      // else {
+      //   resetPassFilter
+      // }
+    }
+  }
+
+  toggleFilterView(name) {
     console.log('asdfasdfasdf')
     this.displayFilters = !this.displayFilters;
   }
--- src/app/resorts/resorts.component.ts
+++ src/app/resorts/resorts.component.ts
@@ -11,8 +11,8 @@ export class ResortsComponent implements OnInit {
   @Input() resorts: Resort[] = [
     new Resort('Alta', 'Little Cottonwood Canyon', 'Utah', 4.8, 'Ski Utah\'s top resort, averages 500 inches of snow a year', 'https://escape2ski.com/wp-content/uploads/2020/06/138-102-Alta-Ski-Area-UT.jpg', 'Ikon', '500'),
     new Resort('Snowbird', 'Little Cottonwood Canyon', 'Utah', 4.7, 'Ski one of utahs top resorts, averages 300 inches of snow per year', 'https://upload.wikimedia.org/wikipedia/commons/6/68/Snowbird_Tram_at_Hidden_Peak.jpg', 'Ikon', '480'),
-    new Resort('Steamboat', 'Steamboat Springs', 'Colorado', 4.6, 'Champagne powder abounds at this tree skiing paradise.', 'https://img6.onthesnow.com/image/xl/73/73931.jpg', 'Ikon', '350'),
-    new Resort('Keystone', 'Summit County', 'Colorado', 4.2, 'Top terrain park in the country', 'https://www.powderhounds.com/site/DefaultSite/filesystem/images/USA/Keystone/Overview/Keystone-12.jpg', 'Ikon', '350'),
+    new Resort('Steamboat', 'Steamboat Springs', 'Colorado', 4.6, 'Champagne powder abounds at this tree skiing paradise.', 'https://img6.onthesnow.com/image/xl/73/73931.jpg', 'Epic', '350'),
+    new Resort('Keystone', 'Summit County', 'Colorado', 4.2, 'Top terrain park in the country', 'https://www.powderhounds.com/site/DefaultSite/filesystem/images/USA/Keystone/Overview/Keystone-12.jpg', 'Epic', '350'),
     new Resort('Niseko', 'Hokkaido', 'Japan', 4.0, 'Powder lovers rejoice.', 'https://i0.wp.com/www.agoda.com/wp-content/uploads/2019/12/Grand-Hirafu-Niseko-Village-ski-resort-things-to-do-in-Niseko-Japan.jpg?ssl=1', 'Ikon', '400'),
     new Resort('Happo Ono', 'Hakuba', 'Japan', 4.0, 'Powder lovers rejoice.', 'https://www.hakuba1.com/english/wp-content/uploads/2015/11/Happo_1.jpg', 'Ikon', '400'),
     new Resort('Jackson Hole', 'Jackson', 'Wyoming', 4.0, 'Big badass skiing.', 'https://cdn.jacksonholenet.com/images/content/14620_G6OZl_Jackson_Hole_Ski_Resorts_lg.jpg', 'Ikon', '400'),
@@ -47,6 +47,16 @@ export class ResortsComponent implements OnInit {
     // check if region/country/or resort name contains string
     let filterArr = this.resorts.filter(resort => resort.name === filterWord);
     console.log(filterArr);
+    this.displayResorts = filterArr;
+  }
+
+  filterResortsByPass(pass: string) {
+    let filterArr = this.resorts.filter(resort => resort.skiPasses === pass);
+    this.displayResorts = filterArr;
+  }
+
+  filterResortsForIkon() {
+    let filterArr = this.resorts.filter(resort => resort.skiPasses === 'Ikon');
     this.displayResorts = filterArr;
   }
 }
