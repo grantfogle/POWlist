36805a3a84be5ad581d8e03be0f39ee30c4f2c2a
Grant <grant.fogle@davita.com>
2020-11-09 18:03:29 -0700

    filter service remove all filters

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -10,6 +10,9 @@ export class FilterService {
     // }
     constructor(public resortsService: ResortsService) {}
     filterBySnowfall(resorts: Resort[]) {}
+    filterByGeo(term: string) {
+        
+    }
     filterByExtremeTerrain() {}
     filterByFamilyFriendly() {}
     filterByAffordability() {}
