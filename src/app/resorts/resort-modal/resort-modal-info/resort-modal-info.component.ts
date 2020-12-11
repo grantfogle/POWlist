3d2650cfd554fab4204afed2012e6b854e64c661
grantfogle <grantjfogle@gmail.com>
2020-12-10 20:01:43 -0700

    incorporating information for resort to modal

--- src/app/resorts/resort-modal/resort-modal-info/resort-modal-info.component.ts
+++ src/app/resorts/resort-modal/resort-modal-info/resort-modal-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Resort } from '../../shared/resort.model';
 
 @Component({
@@ -8,6 +10,7 @@ import { Resort } from '../../shared/resort.model';
 })
 
 export class ResortModalInfoComponent implements OnInit {
+    @Input() id: string;
     resortTable = [
         { name: 'Rating', data: '4.4' },
         { name: 'Snow', data: '400 inches' },
@@ -19,13 +22,53 @@ export class ResortModalInfoComponent implements OnInit {
         { name: 'Side Country Access', data: 'Yes' },
         { name: 'Passes', data: 'Crowded' },
     ];
+    newResortTable = {
+        overallRating: '',
+        lifts: '',
+        passes: '',
+        sideCountryAccess: '',
+        snow: '',
+        terrainBreakdown: '',
+        terrainParks: '',
+        ticketPrices: '',
+        trails: '',
+        vertical: ''
+    }
 
-    constructor() { }
+    constructor(public http: HttpClient) { }
 
     ngOnInit() {
         // this.fetchResortData();
+        this.retrieveResortInfo();
     }
 
+    retrieveResortInfo() {
+        // check for empty fields, if empty fields throw an alert
+        // set a timeout to show then hide alert
+        const url = 'https://powfish.firebaseio.com/resortData.json';
+        this.http.get(
+            url
+        ).subscribe(responseData => {
+            console.log(responseData);
+            for (const resort in responseData) {
+                // console.log('ressssort', responseData[resort]);
+                if (responseData[resort].id === this.id) {
+                    console.log(responseData[resort].terrainBreakdown)
+                    this.newResortTable.overallRating = responseData[resort].overallRating;
+                    this.newResortTable.lifts = responseData[resort].lifts;
+                    this.newResortTable.passes = responseData[resort].passes;
+                    this.newResortTable.sideCountryAccess = responseData[resort].sideCountryAccess;
+                    this.newResortTable.snow = responseData[resort].snow;
+                    this.newResortTable.terrainBreakdown = responseData[resort].terrainBreakdown;
+                    this.newResortTable.terrainParks = responseData[resort].terrainParks;
+                    this.newResortTable.ticketPrices = responseData[resort].ticketPrices;
+                    this.newResortTable.trails = responseData[resort].trails;
+                    this.newResortTable.vertical = responseData[resort].vertical;
+                }
+            }
+            console.log('cats', this.newResortTable);
+        });
+    }
     // get resort data
     // fetchResortData() {
     // this.http.asdfasf
