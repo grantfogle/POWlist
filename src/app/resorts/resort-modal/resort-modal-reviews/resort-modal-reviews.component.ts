b536d89941f9c677b567e14d5fa346f6a3bc8362
grantfogle <grantjfogle@gmail.com>
2020-12-10 21:11:02 -0700

    updating styling of info component

--- src/app/resorts/resort-modal/resort-modal-reviews/resort-modal-reviews.component.ts
+++ src/app/resorts/resort-modal/resort-modal-reviews/resort-modal-reviews.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-resort-modal-reviews',
@@ -10,11 +9,11 @@ import { map } from 'rxjs/operators';
 
 export class ResortModalReviewsComponent implements OnInit {
     @Input() id: string;
-    fbreviews = [];
+    reviews = []
+
     constructor(public http: HttpClient) { }
 
     ngOnInit() {
-        //get reviews for product id
         this.retrieveReviews()
     }
 
@@ -25,9 +24,7 @@ export class ResortModalReviewsComponent implements OnInit {
         this.http.get(
             url
         ).subscribe(responseData => {
-            console.log(responseData);
             for (const review in responseData) {
-                console.log('reeeeeview', responseData[review]);
                 if (responseData[review].resortId === this.id) {
                     let res = {
                         user: 'G money maker',
@@ -37,16 +34,8 @@ export class ResortModalReviewsComponent implements OnInit {
                     }
                     this.reviews.unshift(res);
                 }
-                // if (rev.id)
             }
         });
         return true;
     }
-
-    reviews = [
-        { user: 'Grant  money', description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', ratingDate: 'December 12, 2018', rating: 4 },
-        { user: 'Grant  money', description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', ratingDate: 'December 12, 2018', rating: 3 },
-        { user: 'Grant  money', description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', ratingDate: 'December 12, 2018', rating: 2 },
-        { user: 'Grant  money', description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', ratingDate: 'December 12, 2018', rating: 4 },
-    ]
 }
