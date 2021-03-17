a1fe51cffa2b4cdb5412807e83216e0131eeb6c8
grantfogle <grantjfogle@gmail.com>
2021-03-16 22:56:03 -0600

    adding feedback form submission

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-feedback',
-  templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+    selector: 'app-feedback',
+    templateUrl: './feedback.component.html',
+    styleUrls: ['./feedback.component.css']
 })
 
 export class FeedbackComponent implements OnInit {
     showFeedbackForm = false;
+    showFeedbackThanks = false;
+    feedbackEmail: string = '';
+    feedbackMessage: string = '';
+
     constructor() { }
 
     ngOnInit() {
@@ -15,6 +19,18 @@ export class FeedbackComponent implements OnInit {
 
     toggleFeedbackForm() {
         this.showFeedbackForm = !this.showFeedbackForm;
+    }
+
+    feedbackThanks() {
+        this.showFeedbackThanks = true;
+        setTimeout(() => {
+            this.showFeedbackThanks = false;
+        }, 4500);
+    }
+
+    submitFeedback() {
+        console.log(this.feedbackEmail);
+        console.log(this.feedbackMessage);
     }
 
 }
