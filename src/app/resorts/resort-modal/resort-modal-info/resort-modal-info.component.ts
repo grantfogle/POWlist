19a4673cd8c842aaa587001e6d9058194c286d9a
Grant <grant.fogle@davita.com>
2021-04-07 16:29:18 -0600

    adding updates to modal review rating

--- src/app/resorts/resort-modal/resort-modal-info/resort-modal-info.component.ts
+++ src/app/resorts/resort-modal/resort-modal-info/resort-modal-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Resort } from '../../shared/resort.model';
@@ -11,36 +11,59 @@ import { Resort } from '../../shared/resort.model';
 
 export class ResortModalInfoComponent implements OnInit {
     @Input() id: string;
-    resortReviews: [{
-        // resortId: '123'},
-        overallRating: 4.4}]
-        // snowRating: 4.2,
-        // valueRating: 3.7,
-        // nightLife: 2.2,
-        // crowds: 4.2,
-        // bcAccess: 3.2,
-        // begTerrain: 3.2,
-        // intTerrain: 1.1,
-        // advTerrain: 3.3];
+    @ViewChild('reviewProgressBar') reviewProgress: ElementRef;
+    resortData = [{}];
+
+    // resortReviews: {
+    //     resortId: '123',
+    //     reviewCategories: {
+    //         overall: {name: 'Overall Rating', score: 4.2},
+    //         snow: {name: 'Snow', score: 4.2},
+    //         value: {name: 'Value', score: 2.2},
+    //         nightLife: {name: 'Nightlife', score: 2.4},
+    //         crowds: {name: 'Crowds', score: 3.3},
+    //         bcAccess: {name: 'BC Access', score: 2.5},
+    //         begTerrain: {name: 'Beginner Terrain', score: 2.9},
+    //         intTerrain: {name: 'Intermediate Terrain', score: 3.7},
+    //         advTerrain: {name: 'Advanced Terrain', score: 4.7},
+    //         terrainParks: {name: 'Terrain Parks', score: 3.7}
+    //     }
+    // };
+
+    newResortReviews = {
+        resortId: '123',
+        reviewCategories: [
+            {name: 'Overall Rating', score: 4.2},
+            {name: 'Snow', score: 4.2},
+            {name: 'Value', score: 2.2},
+            {name: 'Nightlife', score: 2.4},
+            {name: 'Crowds', score: 3.3},
+            {name: 'BC Access', score: 2.5},
+            {name: 'Beginner Terrain', score: 2.9},
+            {name: 'Intermediate Terrain', score: 3.7},
+            {name: 'Advanced Terrain', score: 4.7},
+            {name: 'Terrain Parks', score: 3.7}
+        ]
+    };
+
 
     resortTable = [];
-
     newResortTable = {
-        overallRating: '',
-        lifts: '',
-        passes: '',
-        sideCountryAccess: '',
-        snow: '',
-        terrainBreakdown: {
-            green: '',
-            blue: '',
-            black: '',
-            extreme: ''
-        },
-        terrainParks: '',
-        ticketPrices: '',
-        trails: '',
-        vertical: ''
+        "Overall Rating": '',
+        "Lifts": '',
+        "Passes": '',
+        "Side Country Access": '',
+        "Snow": '',
+        // "Terrain Breakdown": {
+        //     green: '',
+        //     blue: '',
+        //     black: '',
+        //     extreme: ''
+        // },
+        "Terrain Parks": '',
+        "Ticket Prices": '',
+        "Trails": '',
+        "Skiable Acres": ''
     }
 
     constructor(public http: HttpClient) { }
@@ -56,23 +79,28 @@ export class ResortModalInfoComponent implements OnInit {
         ).subscribe(responseData => {
             for (const resort in responseData) {
                 if (responseData[resort].id === this.id) {
-                    this.newResortTable.overallRating = responseData[resort].overallRating;
-                    this.newResortTable.lifts = responseData[resort].lifts;
-                    this.newResortTable.passes = responseData[resort].passes;
-                    this.newResortTable.sideCountryAccess = responseData[resort].sideCountryAccess;
-                    this.newResortTable.snow = responseData[resort].snow;
-                    this.newResortTable.terrainBreakdown = responseData[resort].terrainBreakdown;
-                    this.newResortTable.terrainParks = responseData[resort].terrainParks;
-                    this.newResortTable.ticketPrices = responseData[resort].ticketPrices;
-                    this.newResortTable.trails = responseData[resort].trails;
-                    this.newResortTable.vertical = responseData[resort].vertical;
+                    this.newResortTable["Overall Rating"] = responseData[resort].overallRating;
+                    this.newResortTable["Lifts"] = responseData[resort].lifts;
+                    this.newResortTable["Passes"] = responseData[resort].passes;
+                    this.newResortTable["Side Country Access"] = responseData[resort].sideCountryAccess;
+                    this.newResortTable["Snow"] = responseData[resort].snow;
+                    // this.newResortTable["Terrain Breakdown"] = responseData[resort].terrainBreakdown;
+                    this.newResortTable["Terrain Parks"] = responseData[resort].terrainParks;
+                    this.newResortTable["Ticket Prices"] = responseData[resort].ticketPrices;
+                    this.newResortTable["Trails"] = responseData[resort].trails;
+                    this.newResortTable["Skiable Acres"] = responseData[resort].vertical;
+                    console.log('review progress', this.reviewProgress);
                 }
             }
         });
     }
 
-    terrainBreakdown() {
-        return `${this.newResortTable.terrainBreakdown.green} green ${this.newResortTable.terrainBreakdown.blue} blue ${this.newResortTable.terrainBreakdown.black}
-         black ${this.newResortTable.terrainBreakdown.extreme} extreme`;
+    getScoreBarWidth(score) {
+        const percent = (score / 5) * 100;
+        return percent + '%';
     }
+    // terrainBreakdown() {
+    //     return `${this.newResortTable.terrainBreakdown.green} green ${this.newResortTable.terrainBreakdown.blue} blue ${this.newResortTable.terrainBreakdown.black}
+    //      black ${this.newResortTable.terrainBreakdown.extreme} extreme`;
+    // }
 }
