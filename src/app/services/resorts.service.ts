682d47546ba4d12859385cd36a6171e3f2a471c5
grantfogle <grantjfogle@gmail.com>
2021-04-19 22:14:02 -0600

    adding new resorts to data

--- src/app/services/resorts.service.ts
+++ src/app/services/resorts.service.ts
@@ -61,7 +61,7 @@ export class ResortsService {
     retrieveResortsFromDb() {
         // const url = 'https://powfish.firebaseio.com/resorts.json';
         // uncomment to get correct daata
-        const url = 'https://powfish.firebaseio.com/resort2.json';
+        const url = 'https://powfish.firebaseio.com/resorts3.json';
         this.http.get(url)
             .pipe(map(responseData => {
                 const resortsArray = [];
@@ -79,7 +79,7 @@ export class ResortsService {
                 })
                 this.sortResortsByRating();
             })
-        this.getNewJsonDataForGraph();
+        // this.getNewJsonDataForGraph();
     }
 
     filterResortsBySnowfall() {
@@ -95,12 +95,12 @@ export class ResortsService {
     addResort(resort: Resort2) {
         console.log('added resort', resort);
         const url = 'https://powfish.firebaseio.com/new-resort.json';
-            this.http.post(
+        this.http.post(
             url,
             resort
-      ).subscribe(responseData => {
-        console.log(responseData);
-      });
+        ).subscribe(responseData => {
+            console.log(responseData);
+        });
     }
 
     filterResortByWord(searchTerm: string): Resort[] {
@@ -115,77 +115,126 @@ export class ResortsService {
         return filterArr;
     };
 
-/**************
+    /**************
+    
+    GETTING NEW FORMATTED DATA (WILL DELETE)
+    
+    ***************/
 
-GETTING NEW FORMATTED DATA (WILL DELETE)
+    // getNewJsonDataForGraph() {
+    //     let newResortInfo = [];
+    //     // for (const key in resort) {
+    //     let finalArr = [];
+    //     const url = 'https://powfish.firebaseio.com/resorts.json';
+    //     const urlResortInfo = 'https://powfish.firebaseio.com/resortData.json';
+    //     // const url = 'https://powfish.firebaseio.com/resort2.json';
+    //     // 1) GET ALL RESORTS
+    //     // this.http.get(url)
+    //     //     .pipe(map(responseData => {
+    //     //         const resortsArray = [];
+    //     //         console.log('getnewjson data', responseData);
+    //     //         for (const key in responseData) {
+    //     //             if (responseData.hasOwnProperty(key)) {
+    //     //                 resortsArray.push({ ...responseData[key], id: key })
+    //     //             }
+    //     //         }
+    //     //         return resortsArray;
+    //     //     }))
+    //     //     .subscribe(response => {
+    //     //         response.forEach(resort => {
+    //     //             newResorts.push(resort);
+    //     //         })
+    //     //     })
 
-***************/
- 
-    getNewJsonDataForGraph() {
-        let newResortInfo = [];
-        // for (const key in resort) {
-        let finalArr = [];
-        const url = 'https://powfish.firebaseio.com/resorts.json';
-        const urlResortInfo = 'https://powfish.firebaseio.com/resortData.json';
-        // const url = 'https://powfish.firebaseio.com/resort2.json';
-        // 1) GET ALL RESORTS
-        // this.http.get(url)
-        //     .pipe(map(responseData => {
-        //         const resortsArray = [];
-        //         console.log('getnewjson data', responseData);
-        //         for (const key in responseData) {
-        //             if (responseData.hasOwnProperty(key)) {
-        //                 resortsArray.push({ ...responseData[key], id: key })
-        //             }
-        //         }
-        //         return resortsArray;
-        //     }))
-        //     .subscribe(response => {
-        //         response.forEach(resort => {
-        //             newResorts.push(resort);
-        //         })
-        //     })
+    //     // 2) GET ALL RESORTINFO
+    //     this.http.get(urlResortInfo)
+    //         .pipe(map(responseData => {
+    //             const resortsArray = [];
+    //             console.log('get resort data', responseData);
+    //             for (const key in responseData) {
+    //                 if (responseData.hasOwnProperty(key)) {
+    //                     resortsArray.push({ ...responseData[key], name: key })
+    //                 }
+    //             }
+    //             return resortsArray;
+    //         }))
+    //         .subscribe(response => {
+    //             response.forEach(resort => {
+    //                 newResortInfo.push(resort);
+    //             });
+    //             console.log('asdfasdfasfdasf', this.filteredResorts, newResortInfo);
+    //             this.getJsonFormatted(this.filteredResorts, newResortInfo);
+    //             // for (let i = 0; i < this.filteredResorts.length; i++) {
+    //             //     console.log('cats');
+    //             // for (const key in this.filteredResorts[i]) {
+    //             //     console.log(key)
+    //             // [key]: {
+    //             //     name: key.name,
+    //             // } 
+    //         });
 
-        // 2) GET ALL RESORTINFO
-        this.http.get(urlResortInfo)
-            .pipe(map(responseData => {
-                const resortsArray = [];
-                console.log('get resort data', responseData);
-                for (const key in responseData) {
-                    if (responseData.hasOwnProperty(key)) {
-                        resortsArray.push({ ...responseData[key], id: key })
-                    }
-                }
-                return resortsArray;
-            }))
-            .subscribe(response => {
-                response.forEach(resort => {
-                    newResortInfo.push(resort);
-                });
-                console.log('asdfasdfasfdasf', this.filteredResorts, newResortInfo);
-                this.getJsonFormatted(this.filteredResorts, newResortInfo);
-                // for (let i = 0; i < this.filteredResorts.length; i++) {
-                //     console.log('cats');
-            // for (const key in this.filteredResorts[i]) {
-            //     console.log(key)
-                // [key]: {
-                //     name: key.name,
-                // } 
-            });
-        
-        // 3) Combine them into a new array that's convverted into a json
-        // JSON.stringify(obj)
-    }
+    //     // 3) Combine them into a new array that's convverted into a json
+    //     // JSON.stringify(obj)
+    // }
 
-    getJsonFormatted(resortData, resortInfo) {
-        let arr = [];
-        // for (const key in resortData) {
-        //     console.log('asdfaf resort data', resortData);
-        //     if (resortData.hasOwnProperty(key)) {
-        //         arr.push({ ...resortData[key], id: key });
-        //     }
-        // }
-        // console.log('arr', arr);
-    }
+    // getJsonFormatted(resortData, resortInfo) {
+    //     console.log('get formatted json:', resortData);
+    //     console.log('get formatted json:', resortInfo);
+    //     let arr = [];
+
+    //     resortData.forEach(resort => {
+    //         let obj = {};
+    //         resortInfo.forEach(info => {
+    //             if (info.id === resort.id) {
+    //                 obj = {
+    //                     [resort.id]: {
+    //                         name: resort.name,
+    //                         city: resort.city,
+    //                         province: resort.province,
+    //                         country: resort.country,
+    //                         latitude: '',
+    //                         longitude: '',
+    //                         website: '',
+    //                         description: resort.description,
+    //                         coverImage: resort.imagePath,
+    //                         logo: '',
+    //                         cardImage: '',
+    //                         stats: {
+    //                             adultFullDayTicketInUSD: { label: 'Adult 1 Day Ticket', value: info.ticketPrices },
+    //                             bestTimeToVisit: { label: 'Best Time to Visit', value: '' },
+    //                             bikePark: { label: 'Bike Park?', value: '' },
+    //                             lifts: { label: 'Lifts', value: info.lifts },
+    //                             nearestAirportInMiles: { label: 'Nearest Airport (miles)', value: '' },
+    //                             skiableAcres: { label: 'Skiable Acres', value: info.skiableAcres },
+    //                             skiPasses: { label: 'Ski Passes', value: [resort.skiPasses] },
+    //                             sideCountryAccess: { label: 'BC/Sidecountry Access', value: '' },
+    //                             snowPerYearInInches: { label: 'Snow per Year (inches)', value: info.snow },
+    //                             terrainParks: { label: 'Terrain Parks?', value: info.terrainParks },
+    //                             trails: { label: 'Trails', value: info.trails },
+    //                             verticalFeet: { label: 'Vertical Feet', value: info.vertical },
+    //                         },
+    //                         terrainBreakdown: {
+    //                             advTerrainPercentage: { label: 'Advanced Terrain Percentage', value: info.terrainBreakdown.black },
+    //                             begTerrainPercentage: { label: 'Beginner Terrain Percentage', value: info.terrainBreakdown.green },
+    //                             intTerrainPercentage: { label: 'Intermediate Terrain Percentage', value: info.terrainBreakdown.blue },
+    //                             exTerrainPercentage: { label: 'Expert Terrain Percentage', value: info.terrainBreakdown.extreme }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         })
+    //         arr.push(obj);
+    //     })
+
+    //     console.log('ojbbbbjjjj', JSON.stringify(arr));
+    //     // console.log('edited array brah', arr);
+    //     // for (const key in resortData) {
+    //     //     console.log('asdfaf resort data', resortData);
+    //     //     if (resortData.hasOwnProperty(key)) {
+    //     //         arr.push({ ...resortData[key], id: key });
+    //     //     }
+    //     // }
+    //     // console.log('arr', arr);
+    // }
 
 }
